3e7c9991748b62f23a3364e33c42386264935ce1
Pim van Leeuwen <pimvleeuwen@outlook.com>
2024-02-13 11:25:16 +0100

    Completed day 7

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 
@@ -10,18 +11,67 @@ fn main() {
     println!("-- Begin processing --");
 
     part_1(&input);
-    part_2(&input);
 
     println!("-- Done processing  --");
 }
 
 fn part_1(input: &str) {
 
-    println!("Part 1: {}", "max_carrying");
-}   
+    let mut file_stack: Vec<String> = Vec::new();
+    let mut folder_sizes: HashMap<String, u64> = HashMap::new();
 
-fn part_2(input: &str) {
+    for l in input.lines() {
+        if l.starts_with("$ ls") || l.starts_with("dir") {
+            continue; 
+        } else if l.starts_with("$ cd") {
+            let dest: Vec<&str> = l.split_whitespace().collect();
+            let dest = dest[2];
 
+            if dest == ".." {
+                file_stack.pop();
+            } else {
+                if file_stack.is_empty() {
+                    file_stack.push(dest.to_string());
+                } else {
+                    let mut dest_path = file_stack.last().unwrap().to_string();
+                    dest_path.push_str("/");
+                    dest_path.push_str(dest);
 
-    println!("Part 2: {:?}", "&elves[0..3].iter().sum() as &i32");
+                    file_stack.push(dest_path);
+                }
+            }
+        } else {
+            let size: Vec<&str> = l.split_whitespace().collect();
+            let size: u64 = size[0].parse().unwrap();
+            for c in file_stack.clone().into_iter() {
+                let entry = folder_sizes.entry(c).or_insert(0);
+                *entry += size; 
+            }
+        }
+
+    }   
+
+    let mut result: u64 = 0;
+
+    let used_space_now = folder_sizes.get("/").unwrap().clone();
+    let free_space_now = 70000000 - used_space_now;
+    let space_needed = 30000000 - free_space_now;
+    let mut smallest: u64 = 70000000; 
+
+    for (folder, size) in folder_sizes {
+        
+        if size <= 100000 {
+            result += size; 
+        }
+
+        if size > space_needed && size < smallest {
+            smallest = size;
+        }
+    }
+    
+    println!("Part 1: {:?}", result);
+
+    println!("Part 2: {:?}", smallest);
+
 }
+    
---
+++ day_07/target/debug/day_07.d
@@ -0,0 +1,1 @@
+/home/pimvanleeuwen/Documents/aoc_2022/day_07/target/debug/day_07: /home/pimvanleeuwen/Documents/aoc_2022/day_07/src/main.rs
